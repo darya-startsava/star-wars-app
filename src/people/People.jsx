775abc37d1d18f14d44ffaf2fc35cb2150bd6b1e
darya-startsava <daryastartsava@gmail.com>
2024-01-11 04:56:43 +0100

    feat: add responsive cards

--- src/people/People.jsx
+++ src/people/People.jsx
@@ -1,5 +1,14 @@
-import { useGetPeopleQuery } from '../service';
 import { useSelector } from 'react-redux';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Grid,
+  Typography,
+} from '@mui/material';
+import { useGetPeopleQuery } from '../service';
 import { selectFilteredPeople } from './selectors';
 import { Filter as FilterByName } from '../filters/byName/components/Filter';
 import { Filter as FilterByGender } from '../filters/byGender/components/Filter';
@@ -16,13 +25,33 @@ export default function People() {
     <>
       <FilterByName />
       <FilterByGender />
-      <ul>
-        {filteredPeople.map(({ url, name }) => (
-          <li key={url}>
-            <div>{name}</div>
-          </li>
+      <Grid
+        container
+        rowSpacing={{ xs: 1, sm: 2, md: 3 }}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+      >
+        {filteredPeople.map(({ url, name, gender, mass, birth_year }) => (
+          <Grid key={url} item xs={6} sm={4} md={3}>
+            <Card>
+              <CardContent>
+                <CardHeader
+                  title={name}
+                  subheader={`Birth Year: ${birth_year}`}
+                />
+                <Typography>
+                  <ul>
+                    <li>Gender: {gender}</li>
+                    <li>Mass: {mass}</li>
+                  </ul>
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
         ))}
-      </ul>
+      </Grid>
     </>
   );
 }
